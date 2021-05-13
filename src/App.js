9190164dc0c015fe2f15a0014993bada4018ed3f
AGD <56859084+AdarshGDaniel@users.noreply.github.com>
2021-05-14 00:08:53 +0530

    V4

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { useEffect } from 'react';
 import './App.css';
-import { Route, HashRouter, Link, Switch } from "react-router-dom";
+import { Route, HashRouter as Router, Link, Switch } from "react-router-dom";
 import Home from "./Components/Home/Home"
 import ContactUs from './Components/ContactUs/ContactUs'
 import PastEventsPage from './Components/PastEventsPage/PastEventsPage'
 import Rules from './Components/Rules/Rules'
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router'
 
 function App() {
 
-
-
   return (
     <div className="App">
-      <HashRouter  basename="/">
+      <Router>
         <Switch>
           <Route path = "/Rules" component={Rules} />
           <Route path = "/ContactUs" component={ContactUs} />
@@ -23,7 +19,7 @@ function App() {
           <Route path = "/#" component={Home} />
           <Route component={() => (<div>404 Not found 1</div>)} />
         </Switch>
-      </HashRouter>
+      </Router>
     </div>
   );
 }
