0fd0d3190d40e70673649c1ad8c226c5d947fbf6
AGD <56859084+AdarshGDaniel@users.noreply.github.com>
2021-05-13 23:21:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,17 @@ import PastEventsPage from './Components/PastEventsPage/PastEventsPage'
 import Rules from './Components/Rules/Rules'
 
 function App() {
+
+  const history = createHistory({
+    basename: process.env.PUBLIC_URL,
+  });
+
+  const store = configureStore({ history });
+
+
   return (
     <div className="App">
-      <Router>
+      <Router history={history}>
         <Switch>
           <Route path = "/Rules" component={Rules} />
           <Route path = "/ContactUs" component={ContactUs} />
