02ea594f628d5dd4c2691b9a89cac5bda3a22918
AGD <56859084+AdarshGDaniel@users.noreply.github.com>
2021-05-14 00:02:44 +0530

    V3

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
 import React, { useEffect } from 'react';
 import './App.css';
-import { Route, HashRouter as Router, Link, Switch } from "react-router-dom";
+import { Route, HashRouter, Link, Switch } from "react-router-dom";
 import Home from "./Components/Home/Home"
 import ContactUs from './Components/ContactUs/ContactUs'
 import PastEventsPage from './Components/PastEventsPage/PastEventsPage'
 import Rules from './Components/Rules/Rules'
 import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router'
 
 function App() {
 
-  const history = createHistory({
-    basename: process.env.PUBLIC_URL,
-  });
-
-  const store = configureStore({ history });
 
 
   return (
     <div className="App">
-      <Provider store={store}>
-        <Router history={history}>
-          <Switch>
-            <Route path = "/Rules" component={Rules} />
-            <Route path = "/ContactUs" component={ContactUs} />
-            <Route path = "/PastEvents" component={PastEventsPage} />
-            <Route path = "/" component={Home} />
-            <Route path = "/#" component={Home} />
-            <Route component={() => (<div>404 Not found 1</div>)} />
-          </Switch>
-        </Router>
-      </Provider>
+      <HashRouter  basename="/">
+        <Switch>
+          <Route path = "/Rules" component={Rules} />
+          <Route path = "/ContactUs" component={ContactUs} />
+          <Route path = "/PastEvents" component={PastEventsPage} />
+          <Route path = "/" component={Home} />
+          <Route path = "/#" component={Home} />
+          <Route component={() => (<div>404 Not found 1</div>)} />
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
